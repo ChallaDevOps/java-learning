2619e6eda8a55305fbc8ddb3849a918f6fd15662
ChallaDevOps <146905925+ChallaDevOps@users.noreply.github.com>
2024-02-01 22:16:51 +0530

    Printing Datatypes

---
+++ MyStringExample.java
@@ -0,0 +1,25 @@
+public class MyStringExample {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello all..!. Welcome to Java Data type example");
+         System.out.println(1);
+        System.out.println(10);
+        System.out.println(1.8);
+        System.out.println(1);
+        System.out.println('a');
+        System.out.println(-10);
+        System.out.println('$');
+        System.out.println('@');
+        System.out.println(' ');
+        System.out.println("Naresh");
+        System.out.println(" 1 ");
+        System.out.println(" ...... ");
+        System.out.println("  ");
+        System.out.println(true);
+        System.out.println(false);
+        System.out.println("true");
+        System.out.println("true");
+    }
+}
+
+
